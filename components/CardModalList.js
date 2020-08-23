1850a0fea8f7afb77d8559b4e7be4e43367657f9
Ronald <ronald.silva@smarti9.com.br>
2020-08-23 18:18:54 -0300

    GET API IN OBJECT PEOPLE IN PLANET

--- components/CardModalList.js
+++ components/CardModalList.js
@@ -1,16 +1,57 @@
-import React, { useState } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-
+import React,{useState, useEffect} from 'react';
+import { Text, StyleSheet, View, FlatList, Button } from 'react-native';
 
 
 export default  function CardModalList({item}){
+
+  
+  
+
+  const [residentesUrl,setResidentesUrl ] = useState([])
+
+  const residents = item.residents;
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const arrayOfPromises = residents.map(async (api) => await fetch(api));
+  //     const responses = await Promise.all(arrayOfPromises);
+  //     setResidentesUrl(responses);
+  //   })();
+  // }, [])
+
+  useEffect(() => {
+    (async () => {
+      const arrayOfPromises = residents.map((api) => fetch(api).then(apiResponse => apiResponse.json()).then(data => data).catch(console.log));
+      const responses = await Promise.all(arrayOfPromises);
+      setResidentesUrl(responses);
+    })();
+  }, []);
+
+
+
+ useEffect(() => {
+  console.log("AQUI",residentesUrl)
+ },[residentesUrl])
+
+
+
+
+
+ 
+   
+
     return (
       <View style={styles.containerModalList}>
         <Text style={styles.textItem}>Nome: {item.name}</Text>
         <Text style={styles.textItem}>Clima: {item.climate}</Text>
         <Text style={styles.textItem}>Terra: {item.terrain}</Text>
         <Text style={styles.textItem}>População: {item.population} habitantes</Text>  
-        <Text style={styles.textItem}>Massa: {item.diameter}</Text>
+        <Text style={styles.textItem}>Massa: {item.diameter}</Text>  
+        <FlatList
+          data={residentesUrl}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+        
+        />
       </View>
     )
 }
@@ -19,7 +60,7 @@ const styles = StyleSheet.create({
   containerModalList:{
     display:"flex",
     alignItems:"center",
-    fontSize:50
+    fontSize:50,   
   },
   textItem:{
     fontSize:20,
