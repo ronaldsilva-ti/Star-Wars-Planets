087e919b764b0413f3eb100f497585a75cf049e4
Ronald <ronald.silva@smarti9.com.br>
2020-08-23 18:40:27 -0300

    STYLE IN LIST RESIDENTS

--- components/CardModalList.js
+++ components/CardModalList.js
@@ -1,23 +1,12 @@
 import React,{useState, useEffect} from 'react';
-import { Text, StyleSheet, View, FlatList, Button } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 
 export default  function CardModalList({item}){
 
-  
-  
-
   const [residentesUrl,setResidentesUrl ] = useState([])
 
   const residents = item.residents;
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const arrayOfPromises = residents.map(async (api) => await fetch(api));
-  //     const responses = await Promise.all(arrayOfPromises);
-  //     setResidentesUrl(responses);
-  //   })();
-  // }, [])
 
   useEffect(() => {
     (async () => {
@@ -29,27 +18,27 @@ export default  function CardModalList({item}){
 
 
 
- useEffect(() => {
-  console.log("AQUI",residentesUrl)
- },[residentesUrl])
-
-
-
-
-
- 
-   
+//  useEffect(() => {
+//   console.log("AQUI",residentesUrl)
+//  },[residentesUrl])   
 
     return (
       <View style={styles.containerModalList}>
-        <Text style={styles.textItem}>Nome: {item.name}</Text>
+        <Text style={styles.textItem}>Planeta: {item.name}</Text>
         <Text style={styles.textItem}>Clima: {item.climate}</Text>
         <Text style={styles.textItem}>Terra: {item.terrain}</Text>
         <Text style={styles.textItem}>População: {item.population} habitantes</Text>  
-        <Text style={styles.textItem}>Massa: {item.diameter}</Text>  
+        <Text style={styles.textItem}>Massa: {item.diameter}</Text> 
+        <Text style={styles.textItem}>Residentes:</Text>  
+
         <FlatList
           data={residentesUrl}
-          renderItem={({item}) => <Text>{item.name}</Text>}
+          renderItem={({item}) => (
+            <View style={styles.containerResidents}>
+              <Text style={styles.residents}>Nome: {item.name}</Text>
+              <Text style={styles.residents}>Massa: {item.mass  === 'unknown' ? 'Indefenida' : item.mass}</Text>
+            </View>
+          )}
         
         />
       </View>
@@ -67,5 +56,16 @@ const styles = StyleSheet.create({
     color:'#EBFF00',
     marginTop:10,
     textTransform:'uppercase'
+  },
+  containerResidents:{     
+    borderWidth:2,
+    margin:2,
+    marginTop:10
+  },  
+  residents:{
+    textTransform:'uppercase',
+    color:'white',
+    textAlign:'center',   
+    margin:5
   }
 })
